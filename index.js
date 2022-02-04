1811c323dd6ececa937994d57fb4ff805d5dd096
Soohyun Lee <84074670+topnotchsh@users.noreply.github.com>
2022-02-05 00:35:54 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://soohyunlee:1670@boilerplate.xftec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  //useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요:)')
 })
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}!`)
 })
